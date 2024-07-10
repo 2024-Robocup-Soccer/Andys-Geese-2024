bd51a2e22dbd753ee6277141db234d11aa1ecb8f
Matteo Cericola <128947575+Sportsman7101@users.noreply.github.com>
2024-07-10 11:39:07 -0400

    Add current working bottom code
    Stays at initial angle using gyro, uses sine wave to move, and once the ball is at sensor 12 or 0 degrees, it moves forward at full speed

---
+++ 4-21-24 Soccer Code/Bottom Code/motorSine.ino
@@ -0,0 +1,184 @@
+#include "motorControlV3.h"
+#include <RunningMedian.h>
+
+const float pi = PI;
+
+
+// Returns motor value according to trig function associated with the ball angle. If returns
+// -10.00, it didn't work (as function is between 1 --> -1)
+float movementSineFunction(int motor, float ballAngleDegrees, boolean isCounter, int amplitude) {
+    float motorValue;
+    float ballAngle = pi/180 * ballAngleDegrees;
+    switch (motor) {
+        case 1:
+            motorValue = amplitude * sin(ballAngle + PI / 4);
+            // motorValue = amplitude * sin(ballAngle);
+            break;
+        case 2:
+            motorValue = amplitude * sin(ballAngle - PI / 4);
+            // motorValue = amplitude * sin(ballAngle - PI/2);
+            break;
+        case 3:
+            motorValue = amplitude * sin(ballAngle - 3 * PI / 4);
+            // motorValue = amplitude * -1 * sin(ballAngle - PI/2);
+            break;
+        case 4:
+            motorValue = amplitude * sin(ballAngle + 3 * PI / 4);
+            // motorValue = amplitude * -1 * sin(ballAngle - PI/2);
+            break;
+        default: motorValue = 0;
+        
+    }
+
+    if (isCounter) {
+        motorValue *= -1;
+    }
+
+    return motorValue;
+}
+
+int calculateAngleError(int desiredDegrees, int actualDegrees) {
+    int error = desiredDegrees - actualDegrees;
+
+    if (error > 180) {
+        error -= 360;
+    } else if (error < -180) {
+        error += 360;
+    }
+
+    return error;
+}
+
+int timeZero, currentMillis, pastMillis;
+float ballAngleErrorSum = 0.00, currentBallAngleError = 0.00, pastBallAngleError, ballAngleErrorChange;
+
+Motor motorController;
+void setup() {
+    Serial.begin(9600);
+    Serial2.begin(9600);
+    timeZero = millis();
+
+    motorController.initMotors();
+}
+
+//variables needed for PID
+float kPBall[4] = {1.00, 1.00, 1.00, 1.00};
+float kIBall[4] = {1.00, 1.00, 1.00, 1.00};
+float kDBall[4] = {1.00, 1.00, 1.00, 1.00};
+
+float desiredBallAngleDegrees = 0.00, currentBallAngleDegrees = 0.00;
+
+int motorValues[4];
+
+int information;
+int i =0;
+boolean calibrated = false;
+int ballAngle, netAngle, ballDistance, robotAngle;
+int inputType = 0;
+
+RunningMedian ballAngles = RunningMedian(5);
+int speed;
+
+int angleValue;
+int pKRobotAngle = 1.00, iKRobotAngle = 0.0, dKRobotAngle = 0.0;
+int robotDesiredAngle = 0;
+int robotAngleError = 0;
+int pastRobotAngleError;
+int iRobotAngleError;
+int dRobotAngleError;
+
+boolean isMovingCounterClockwise = true; 
+// Runs the movementSineFunction keeping the ball in front with PID controller
+// Desired position can be changed into a parameter but only if you don't want
+// the ball in front of the bot
+void loop() {
+        if(Serial2.available() > 0){
+        information = Serial2.read();
+        switch (inputType) {
+        case 0:
+            inputType++;
+            break;
+        case 1:
+            robotAngle = information;
+            inputType++;
+            break;
+        case 2:
+            currentBallAngleDegrees = information * 360/12;
+            inputType++;
+            break;
+        case 3:
+            ballAngle = information;
+            inputType++;
+            break;
+        case 4:
+            netAngle = information;
+            inputType = 0;
+            break;
+        }
+    }
+    else {
+        // Serial.println("N/A");
+    }
+    // Serial.print("angle: ");
+    // Serial.print(robotAngle);
+    // Serial.print(" ball distance: ");
+    // Serial.print(ballDistance);
+    // Serial.print(" ball angle: ");
+    Serial.println(ballAngle);
+    // Serial.print(" net angle: ");
+    // Serial.println(netAngle);
+
+    ballAngles.add(ballAngle * 360/12);
+    currentBallAngleDegrees = ballAngles.getMedian();
+    pastBallAngleError = currentBallAngleError;
+    currentBallAngleError = calculateAngleError(desiredBallAngleDegrees, currentBallAngleDegrees);
+
+    //setting current and past time for integration and differenciation
+    pastMillis = currentMillis;
+    currentMillis = millis() - timeZero;
+
+    //calculating integral
+    ballAngleErrorSum += currentBallAngleError * (currentMillis - pastMillis);
+
+    //calculating derivative
+    ballAngleErrorChange = (currentBallAngleError - pastBallAngleError) / (currentMillis - pastMillis);
+    speed = 200;
+    
+    if(currentBallAngleDegrees == 0) {
+        motorValues[0] = -255;
+        motorValues[1] = -255;
+        motorValues[2] = 255;
+        motorValues[3] = 255;
+    }
+    else {
+        if(currentBallAngleDegrees > 180) isMovingCounterClockwise = true;
+        else isMovingCounterClockwise = false;
+        for(int i = 0; i < 4; i++) {
+            motorValues[i] = movementSineFunction(i+1, currentBallAngleDegrees, isMovingCounterClockwise, speed);
+            Serial.print(motorValues[i]);
+            Serial.print(" ");
+            // Serial.print(currentBallAngleDegrees);
+            // Serial.print(" ");
+            // Serial.print(isMovingCounterClockwise);
+            // Serial.print(" ");
+            // Serial.println(speed);
+        }
+    }
+    
+
+    pastRobotAngleError = robotAngleError;
+    robotAngleError = calculateAngleError(robotDesiredAngle, robotAngle);
+
+    pastMillis = currentMillis;
+    currentMillis = millis() - timeZero;
+    iRobotAngleError += robotAngleError * (currentMillis - pastMillis);
+    dRobotAngleError = (robotAngleError - pastRobotAngleError)/(currentMillis - pastMillis);
+    // motorValue = pK*angleError;
+    for(int i = 0; i < 4; i++) {
+        motorValues[i] += pKRobotAngle*robotAngleError + iKRobotAngle*iRobotAngleError + dKRobotAngle*dRobotAngleError;
+    }  
+    Serial.println("");
+    motorController.moveRobot(motorValues[0], motorValues[1], motorValues[2], motorValues[3]);
+    // motorController.moveRobot(speed, speed, -1 * speed, -1 * speed);
+
+}
