ff5a851adec742f2bc7a7be80bb6110f7f5e4beb
Matteo Cericola <128947575+Sportsman7101@users.noreply.github.com>
2024-07-10 15:33:18 -0400

    Add files via upload

---
+++ 2024 Soccer/Bottom Code/BNO.ino
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include <Adafruit.h>
+
+// For SPI mode, we need a CS pin
+#define BNO08X_CS 10
+#define BNO08X_INT 9
+
+// For SPI mode, we also need a RESET
+// #define BNO08X_RESET 5
+// but not for I2C or UART
+#define BNO08X_RESET -1
+
+Adafruit_BNO08x bno08x(BNO08X_RESET);
+sh2_SensorValue_t sensorValue;
+
+
+Compass::Compass() {
+}
+
+void Compass::setReports(void) {
+    Serial.println("Setting desired reports");
+    if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+        Serial.println("Could not enable game vector");
+    }
+}
+
+void Compass::initPrint() {
+    if (!bno08x.begin_UART(&Serial1)) {
+        Serial.println("Failed to find BNO08x chip");
+        while (1) {
+            delay(10);
+        }
+    }
+    Serial.println("Setting desired reports");
+    if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+        Serial.println("Could not enable game vector");
+    }
+}
+
+void Compass::resetCheck() {
+   if (bno08x.wasReset()) {
+        if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+        }
+    }
+
+    if (!bno08x.getSensorEvent(&sensorValue)) {
+        return;
+    } 
+}
+
+void Compass::resetCheckPrint() {
+    if (bno08x.wasReset()) {
+        Serial.print("sensor was reset ");
+        Serial.println("Setting desired reports");
+        if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
+            Serial.println("Could not enable game vector");
+        }
+    }
+
+    if (!bno08x.getSensorEvent(&sensorValue)) {
+        return;
+    }
+}
+
+float Compass::getBallAngle() {
+    return sensorValue.un.gameRotationVector.k;
+}
