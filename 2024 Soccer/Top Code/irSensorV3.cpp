bd1c64032818af69e25a9b03cc75f02ad6dfc8e5
Yuan Yoshimi <139490234+YuanYoshimi@users.noreply.github.com>
2024-07-12 10:10:18 -0400

    Update irSensorV3.cpp

--- 2024 Soccer/Top Code/irSensorV3.cpp
+++ 2024 Soccer/Top Code/irSensorV3.cpp
@@ -1,8 +1,23 @@
 #include "irSensorV3.h"
+#include <RunningMedian.h>
 
 IRSensor::IRSensor() 
 : s1Distance(sensorReadCount), s2Distance(sensorReadCount), s3Distance(sensorReadCount), s4Distance(sensorReadCount), s5Distance(sensorReadCount), s6Distance(sensorReadCount), s7Distance(sensorReadCount), s8Distance(sensorReadCount), s9Distance(sensorReadCount), s10Distance(sensorReadCount), s11Distance(sensorReadCount), s12Distance(sensorReadCount) {
 }
+
+
+const int numberMedianValues = 25;
+RunningMedian sDistances[12] = {RunningMedian(numberMedianValues), RunningMedian(numberMedianValues), RunningMedian(numberMedianValues), 
+RunningMedian(numberMedianValues), RunningMedian(numberMedianValues), RunningMedian(numberMedianValues),RunningMedian(numberMedianValues), 
+RunningMedian(numberMedianValues), RunningMedian(numberMedianValues), RunningMedian(numberMedianValues), RunningMedian(numberMedianValues), 
+RunningMedian(numberMedianValues)};
+
+
+int lowestPin[numberMedianValues];
+int IR_PINS[12] = {A11, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10};
+//12, 1, 2, 3 ... 11
+int sensorValues[12];
+
 
 void IRSensor::readSensors() {
     for (int i = 0; i < sensorReadCount; i++) {
@@ -79,4 +94,150 @@ void IRSensor::debugValues() {
       Serial.println(y);
     }
   }
+
+  
+float IRSensor::findSensorDistance(int sensor, int intensity) {
+    float distance = -1;
+    switch (sensor) {
+        case 0:
+             //distance = ; //Bot 1
+             distance = 1.9214*intensity - 81.673;//Bot 2
+             //distance = ;//Bot 3
+            break;
+        case 1:
+            //distance = ;//Bot 1
+            distance = 1.6369*intensity - 72.97;//Bot 2
+            //distance = ;//Bot 3
+            break;
+        case 2:
+            //distance = ;//Bot 1   
+            distance = 1.5889*intensity - 65.907;//Bot 2
+            //distance = ;//Bot 3
+            break;
+        case 3:
+            //distance = ;//Bot 1
+            distance = 1.8732*intensity - 78.922;//Bot 2
+            //distance = ;//Bot 3
+            break;
+        case 4:
+            //distance = ;//Bot 1
+            distance = 2.1074*intensity - 86.835;//Bot 2
+            //distance = ;//Bot 3
+            break;
+
+        case 5:
+            //distance = ;//Bot 1
+           distance = 1.9093*intensity - 80.002;//Bot 2
+           //distance = ;//Bot 3
+           break;
+        case 6:
+            //distance = ;//Bot 1
+           distance = 1.6892*intensity - 71.084;//Bot 2
+           //distance = ;//Bot 3
+           break;
+        case 7:
+            //distance = ;//Bot 1
+            distance = 1.7187*intensity - 73.411;//Bot 2
+            //distance = ;//Bot 3
+           break;
+        case 8:
+            //distance = ;//Bot 1
+           distance = 1.7463*intensity - 76.2;//Bot 2
+           //distance = ;//Bot 3
+           break;
+        case 9:
+            //distance = ;//Bot 1
+            distance = 1.6595*intensity - 68.838;//Bot 2
+            //distance = ;//Bot 3
+            break;
+        case 10:
+            //distance = ;//Bot 1
+            distance = 1.7076*intensity - 71.065;//Bot 2
+            //distance = ;//Bot 3
+            break;        
+        case 11:
+            //distance = ;//Bot 1
+            distance = 1.626*intensity - 67.416;//Bot 2
+            //distance = ;//Bot 3
+            break;
+    }
+    return distance;
+}
+
+
+//returns the which sensor the ball is closest to
+int IRSensor::getBallAngle(){
+    int lowestSensor =100000;
+    int sensor;
+    for(int i=0; i<12;i++){
+        int intensity = analogRead(IR_PINS[i]);
+        int distance = findSensorDistance(i,intensity);
+        if(distance < lowestSensor){
+            lowestSensor = distance;
+            sensor = i;
+        }
+    }
+    return sensor;
+}
+
+
+//puts raw data into curve then takes median
+void IRSensor::readCalibratedSensors() {
+    int min;
+    int minIndex;
+    for(int i = 0; i < numberMedianValues; i++) {
+        min = 1e8;
+        minIndex = -1;
+        for(int j = 0; j < 12; j++) {
+            sensorValues[j] = analogRead(IR_PINS[j]);
+            sDistances[j].add(findSensorDistance(j, sensorValues[j]));
+            if(sensorValues[j] < min) {
+                minIndex = j;
+                min = sensorValues[j];
+            }
+        }
+        if(minIndex != -1) lowestPin[i] = (minIndex);
+    }
+}
+
+
+//Works by adding the compenents of the angle of the sensor as if it was a unit circle
+//then diving by the number of sensor values
+float IRSensor::calculateCircularAverage() {
+    float sinSum = 0.0;
+    float cosSum = 0.0;
+    float radiansPerValue = (2.0 * 3.14) / 12.0;
+
+    for(int i = 0; i < numberMedianValues; i++) {
+        float angle = sensorValues[i] * radiansPerValue;
+        sinSum += sin(angle);
+        cosSum += cos(angle);
+    }
+
+    float avgAngle = atan2(sinSum / numberMedianValues, cosSum / numberMedianValues);
+
+    if (avgAngle < 0) {
+        avgAngle += 2.0 * 3.14;
+    }
+
+    float averageValue = avgAngle / radiansPerValue;
+    return averageValue;
+}
+
+
+float IRSensor::calculateSensorError(int desiredSensor, int actualSensor) {
+    int error = desiredSensor - actualSensor;
+    
+    if (error > 11) {
+        error -= 11;
+    } else if (error < 0) {
+        error += 11;
+    }
+
+    return error;
+}
+
+int IRSensor::getIntensity(int ballAngle){
+    return analogRead(IR_PINS[ballAngle]);
+}
 
