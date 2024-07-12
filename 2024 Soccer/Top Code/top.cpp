f88d88613fcfc456e281f350e3a65896d66b6f6a
Yuan Yoshimi <139490234+YuanYoshimi@users.noreply.github.com>
2024-07-12 11:17:28 -0400

    Update top.cpp

--- 2024 Soccer/Top Code/top.cpp
+++ 2024 Soccer/Top Code/top.cpp
@@ -4,29 +4,69 @@
 #include <RunningMedian.h>
 #include <Math.h>
 #include "irSensorV3.h"
-#include "Compass.h"
+
 
 IRSensor irSensor;
-Compass compass;
 
+#define BNO08X_CS 10
+#define BNO08X_INT 9
+#define BNO08X_RESET -1
+Adafruit_BNO08x bno08x(BNO08X_RESET);
+sh2_SensorValue_t sensorValue;
+
+
+//Used for setting up the compass
+void setReports(void)
+{
+    Serial.println("Setting desired reports");
+    if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR))
+    {
+        Serial.println("Could not enable game vector");
+    }
+}
+
+//returns the compass angle
+float getBotAngle()
+{
+    return sensorValue.un.gameRotationVector.k;
+}
 
 //setup
-void setup()
-{
+void setup(){
 
     Serial.begin(9600);
     Serial7.begin(9600);
-    compass.init();
-    compass.resetCheckPrint();
-    compass.setReports();
-    //delay(100);
+    if (!bno08x.begin_UART(&Serial1))
+    {
+        Serial.println("Failed to find BNO08x chip");
+        while (1)
+        {
+            delay(10);
+        }
+    }
+    setReports();
+    delay(100);
 }
 
- 
+//Initialize the Compass
+void compassInit() {
+     if (bno08x.wasReset())
+    {
+        Serial.print("sensor was reset ");
+        setReports();
+    }
+
+    if (!bno08x.getSensorEvent(&sensorValue))
+    {
+        return;
+    }
+}
+
 void loop()
 {
+    compassInit();
     int val = pulseIn(10, HIGH, 1000);
-    int compassAngle = compass.getBotAngle();
+    int compassAngle = getBotAngle()*100;
     int ballAngle = irSensor.getBallAngle();
     int intensity = irSensor.getIntensity(ballAngle);
     int distance = irSensor.findSensorDistance(ballAngle, intensity);
@@ -35,11 +75,18 @@ void loop()
     Serial7.write(compassAngle);
     Serial7.write(distance);
     Serial7.write(ballAngle);
-
     // 0 = no info from the camera
     // 1-250 = angle value of goal relative to robot
     // 500 = no goal found
     if (val>0){ Serial7.write(val);}
-    else{
-        Serial7.write(-1);}//No Info from Camera
+    else{Serial7.write(-1);}//No Info from Camera
+
+    // Serial.print("compass angle: ");
+    // Serial.println(compassAngle);
+    // Serial.print(" ball distance: ");
+    // Serial.println(distance);
+    // Serial.print(" ball angle: ");
+    // Serial.print(ballAngle);
+    // Serial.print(" net angle: ");
+    // Serial.println(val);
 }
