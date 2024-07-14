35a14d12648b4bcf902ef53c82dd57b66a8792ad
Yuan Yoshimi <139490234+YuanYoshimi@users.noreply.github.com>
2024-07-14 16:40:56 -0400

    Update top.cpp

--- 2024 Soccer/Top Code/top.cpp
+++ 2024 Soccer/Top Code/top.cpp
@@ -5,62 +5,30 @@
 #include <Math.h>
 #include "irSensorV3.h"
 
-
-IRSensor irSensor;
-
 #define BNO08X_CS 10
 #define BNO08X_INT 9
 #define BNO08X_RESET -1
+
+IRSensor irSensor;
 Adafruit_BNO08x bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
 
 
 //Used for setting up the compass
-void setReports(void)
-{
+void setReports(void) {
     Serial.println("Setting desired reports");
-    if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR))
-    {
-        Serial.println("Could not enable game vector");
-    }
+    if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {Serial.println("Could not enable game vector");}
 }
 
 //returns the compass angle
-float getBotAngle()
-{
-    return sensorValue.un.gameRotationVector.k;
-}
-
-//setup
-void setup(){
-
-    Serial.begin(9600);
-    Serial7.begin(9600);
-    if (!bno08x.begin_UART(&Serial1))
-    {
-        //Serial.println("Failed to find BNO08x chip");
-        while (1)
-        {
-            delay(10);
-        }
-    }
-    setReports();
-    delay(100);
-}
+float getBotAngle() {return sensorValue.un.gameRotationVector.k;}
 
 //Initialize the Compass
 void compassInit() {
-     if (bno08x.wasReset())
-    {
-        //Serial.print("sensor was reset ");
-        setReports();
-    // }
-
-    if (!bno08x.getSensorEvent(&sensorValue))
-    {
-        return;
-    }
+    if (bno08x.wasReset()) {setReports();}
+    if (!bno08x.getSensorEvent(&sensorValue)) {return;}
 }
+
 //type 1 = compass, 2 = ball distance, 3 = ball angle, 4 = net angle
 int pack(int value, int type) {
     int info = -1;
@@ -81,33 +49,48 @@ int pack(int value, int type) {
     return info;
 }
 
+//setup
+void setup(){
+
+     Serial.begin(9600);
+    Serial7.begin(20000000);
+    if (!bno08x.begin_UART(&Serial1)) {
+        Serial.println("Failed to find BNO08x chip");
+        while (1) {
+            delay(10);
+        }
+    }
+    setReports();
+    delay(100);
+}
 
 void loop() {
     compassInit();
-    int val = pulseIn(10, HIGH, 1000);
-    int compassAngle = getBotAngle()*100;
-    int ballAngle = irSensor.getBallAngle();
-    int intensity = irSensor.getIntensity(ballAngle);
-    int distance = irSensor.findSensorDistance(ballAngle, intensity);
-
-    Serial7.write(255);
-    Serial7.write(pack(compassAngle, 1));
-    Serial7.write(pack(distance, 2));
-    Serial7.write(pack(ballAngle, 3));
-    // 0 = no info from the camera
-    // 1-250 = angle value of goal relative to robot
-    // 500 = no goal found
-    if (val>0){ Serial7.write(pack(val,4));}
-    else{Serial7.write(-1);}//No Info from Camera
-
-
     
-    // Serial.print("compass angle: ");
-    // Serial.println(pack(compassAngle, 1));
-    // Serial.print(" ball distance: ");
-    // Serial.println(distance);
-    // Serial.print(" ball angle: ");
-    // Serial.println(ballAngle);
-    // Serial.print(" net angle: ");
-    // Serial.println(val);
+    u_int8_t compassAngle = pack(getBotAngle()*100, 1);
+    u_int8_t ballAngle = pack(irSensor.getBallAngle(), 3);
+    u_int8_t intensity = irSensor.getIntensity(ballAngle);
+    u_int8_t distance = pack(irSensor.findSensorDistance(ballAngle, intensity), 2);
+
+    int val = pulseIn(10, HIGH, 600);
+    if (val > 0) {
+        if(val > 250) {
+            val = 253;
+        }
+    }
+    else {
+        val = 0;
+    }
+    
+    u_int8_t information[5] = { 255, compassAngle, distance, ballAngle, u_int8_t(val) };
+    Serial7.write(information, 5);
+   
+    Serial.print("compass angle: ");
+    Serial.print(information[1]);
+    Serial.print(" ball distance: ");
+    Serial.print(information[2]);
+    Serial.print(" ball angle: ");
+    Serial.print(information[3]);
+    Serial.print(" net angle: ");
+    Serial.println(information[4]);
 }
