02105d5c472770d69681ad394cb057f3fc7a4878
Yuan Yoshimi <139490234+YuanYoshimi@users.noreply.github.com>
2024-07-14 22:43:45 -0400

    Update top.cpp

--- 2024 Soccer/Top Code/top.cpp
+++ 2024 Soccer/Top Code/top.cpp
@@ -1,27 +1,27 @@
-//Adding Libraries
+//---------Libraries---------
 #include <Arduino.h>
 #include <Adafruit_BNO08x.h>
 #include <RunningMedian.h>
 #include <Math.h>
 #include "irSensorV3.h"
 
+//---------Compass Pins---------
 #define BNO08X_CS 10
 #define BNO08X_INT 9
 #define BNO08X_RESET -1
 
+//---------Initializing Libraries---------
 IRSensor irSensor;
 Adafruit_BNO08x bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
+RunningMedian ballAngleMedian = RunningMedian(5);
 
-
+//REQUIRED FOR COMPASS SETUP
 //Used for setting up the compass
 void setReports(void) {
     Serial.println("Setting desired reports");
     if (!bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {Serial.println("Could not enable game vector");}
 }
-
-//returns the compass angle
-float getBotAngle() {return sensorValue.un.gameRotationVector.k;}
 
 //Initialize the Compass
 void compassInit() {
@@ -29,6 +29,11 @@ void compassInit() {
     if (!bno08x.getSensorEvent(&sensorValue)) {return;}
 }
 
+
+//Returns the a int from 0-11 for which sensor the ball is closest to. 
+float getBotAngle() {return sensorValue.un.gameRotationVector.k;}
+
+//Packs the data so it can be sent down
 //type 1 = compass, 2 = ball distance, 3 = ball angle, 4 = net angle
 int pack(int value, int type) {
     int info = -1;
@@ -51,9 +56,11 @@ int pack(int value, int type) {
 
 //setup
 void setup(){
-
-     Serial.begin(9600);
+//--------Starting Serial-----------
+    Serial.begin(9600);
     Serial7.begin(20000000);
+
+//-------Starting Compass UART-----------
     if (!bno08x.begin_UART(&Serial1)) {
         Serial.println("Failed to find BNO08x chip");
         while (1) {
@@ -65,26 +72,33 @@ void setup(){
 }
 
 void loop() {
-    compassInit();
+
+    compassInit();//Compass
     
+    //---------------Setting up Variables---------------
     u_int8_t compassAngle = pack(getBotAngle()*100, 1);
-    u_int8_t ballAngle = pack(irSensor.getBallAngle(), 3);
+    u_int8_t ballAngle = pack(ballAngleMedian.getMedian(), 3);
     u_int8_t intensity = irSensor.getIntensity(ballAngle);
     u_int8_t distance = pack(irSensor.findSensorDistance(ballAngle, intensity), 2);
 
-    int val = pulseIn(10, HIGH, 600);
-    if (val > 0) {
-        if(val > 250) {
-            val = 253;
-        }
-    }
-    else {
-        val = 0;
-    }
+    //Getting the RunningMedian of the BallAngle
+    ballAngleMedian.add(irSensor.getBallAngle());
+
+    //Reading value from Camera
+    int cameraValue = pulseIn(10, HIGH, 600);
+
+    //If the camera sends down anything above 0. It means that it is getting valid data from Camera
+    //If camera sends anything above 250. It means that it cannot see the net and will set it to 253. 
+    if (cameraValue > 0) {if(cameraValue > 250) {cameraValue = 253;}}
+    else {cameraValue = 0;}
     
-    u_int8_t information[5] = { 255, compassAngle, distance, ballAngle, u_int8_t(val) };
+    //Putting all the data into a array to send down
+    u_int8_t information[5] = { 255, compassAngle, distance, ballAngle, u_int8_t(cameraValue) };
+    
+    //Sending down the Data down
     Serial7.write(information, 5);
-   
+
+    //--------DeBug/Read Data-----------
     Serial.print("compass angle: ");
     Serial.print(information[1]);
     Serial.print(" ball distance: ");
