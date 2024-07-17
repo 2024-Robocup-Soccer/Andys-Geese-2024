e799f54b8666c29689b5060e546c09e4467d7705
Matteo Cericola <128947575+Sportsman7101@users.noreply.github.com>
2024-07-17 04:29:42 -0400

    Update readValuesBottom.cpp

--- Calibration Code/readValuesBottom.cpp
+++ Calibration Code/readValuesBottom.cpp
@@ -1,93 +1,87 @@
-// #include <Arduino.h>
-// #include <SPI.h>
-// int information;
-// int inputType = 0;
-// int ballAngle, netAngle, ballDistance, robotAngle;
-// float desiredBallAngleDegrees = 0.00, currentBallAngleDegrees = 0.00;
+#include <Arduino.h>
+#include <SPI.h>
 
-// int unpackData(int value, int type) {
-//     int info = -1;
-//     if(type == 1) {
-//         // info = map(value, 0, 250, 0, 360);
-//         info = value;
-//     }
-//     //not done
-//     else if(type == 2) {
-//         info = value;
-//     }
-//     else if(type == 3) {
-//          info = map(value, 0, 250, 0, 360);
-//         //info = value;
-//     }
-//     else if(type == 4) {
-//         info = map(value, 0, 250, -50, 50);
-//     }
-//     return info;
-// }
+int inputType = 0;
+int ballAngle =- 1, netAngle, ballDistance, robotAngle;
+float desiredBallAngleDegrees = 0.00, currentBallAngleDegrees = 0.00;
 
-// void setup() {
-//     Serial.begin(9600);
-//     Serial2.begin(9600);
-// }
+//COMMUNICATION
+byte information[5];
+//Allows the top and bottom boards to recieve info properly
+//Needs to be run at top of communication in loop
+void sync() {
+    byte check[5];
+    int shift=0;
+    if(Serial2.available() > 4) {
+        Serial2.readBytes(check, 5);
+    }
 
-// void loop() {
-//     // if(Serial2.available() > 0){
-//     //     information = Serial2.read();
-//     //     switch (inputType) {
-//     //     case 0:
-//     //         if(information == 255){inputType++;}
-//     //         break;
-//     //     case 1:
-//     //         robotAngle = information;
-//     //         inputType++;
-//     //         break;
-//     //     case 2:
-//     //         ballDistance = information;
-//     //         inputType++;
-//     //         break;
-//     //     case 3:
-//     //         ballAngle = information;
-//     //         inputType++;
-//     //         break;
-//     //     case 4:
-//     //         netAngle = information;
-//     //         inputType = 0;
-//     //         break;
-//     //     }
-//     // }
+    for (int i=0; i<5; i++) {
+        if (check[i] == 255) {
+            shift = i;
+        }
+    }
 
-//      if(Serial2.available() > 0) {
-//         information = Serial2.read();
-//         switch (inputType) {
-//         case 0:
-//             if(information == 255) inputType++;
-//             break;
-//         case 1:
-//             robotAngle = unpackData(information, 1);
-//             inputType++;
-//             break;
-//         case 2:
-//             ballDistance = unpackData(information,2);
-//             inputType++;
-//             break;
-//         case 3:
-//             ballAngle = unpackData(information,3);
-//             inputType++;
-//             break;
-//         case 4:
-//             netAngle = unpackData(information,4);
-//             inputType = 0;
-//             break;
-//         }
-//     }
+    if (shift > 0) {
+        shift -= 1;
+    }
 
-//     Serial.print("angle: ");
-//     Serial.print(robotAngle);
-//     Serial.print(" ball distance: ");
-//     Serial.print(ballDistance);
-//     Serial.print(" ball angle: ");
-//     Serial.print(ballAngle);
-//     Serial.print(" net angle: ");
-//     Serial.println(netAngle);
+    for (int i=0; i<shift; i++) {
+        Serial2.read();
+    }
 
-// }
+}
+
+int unpackData(int value, int type) {
+    int info = -1;
+    if(type == 1) {
+        // info = map(value, 0, 250, 0, 360);
+        info = value;
+    }
+    //not done
+    else if(type == 2) {
+        info = value;
+    }
+    else if(type == 3) {
+         info = map(value, 0, 250, 0, 360);
+        //info = value;
+    }
+    else if(type == 4) {
+        info = map(value, 0, 250, -50, 50);
+    }
+    return info;
+}
+
+
+
+void setup() {
+    Serial.begin(9600);
+    Serial2.begin(20000000);
+    Serial.println("Geood");
+}
+
+void loop() {
+    //communication
+    sync();
+
+    if(Serial2.available() > 4) {
+        Serial2.readBytes(information, 5);
+    }
+
+    robotAngle = unpackData(information[1], 1);
+    ballDistance = unpackData(information[2], 2);
+    ballAngle = unpackData(information[3], 3);
+    netAngle = unpackData(information[4], 4);
+    
+    Serial.println(ballAngle);
+
+    Serial.print("angle: ");
+    Serial.print(robotAngle);
+    Serial.print(" ball distance: ");
+    Serial.print(ballDistance);
+    Serial.print(" ball angle: ");
+    Serial.print(ballAngle);
+    Serial.print(" net angle: ");
+    Serial.println(netAngle);
+
+}
