67ff870042ee5291c25a3ecbd0741b176ad1c2fa
Yuan Yoshimi <139490234+YuanYoshimi@users.noreply.github.com>
2024-07-19 00:12:32 +0200

    Create irSensorV3.h

---
+++ Day 3 Code/Libraries/irSensorV3.h
@@ -0,0 +1,32 @@
+#ifndef IRSENSORV3_H
+#define IRSENSORV3_H
+
+#include <Arduino.h>
+#include <RunningMedian.h>
+
+class IRSensor {
+public:
+    IRSensor();
+    void readSensors();
+    int findMinSensor();
+    int findBallDirection();
+    void printBallDirection();
+    void debugValues();
+    void debugComponents();
+    float findSensorDistance(int sensor, int intensity);
+    int getBallAngle();
+    float calculateCircularAverage();
+    float calculateSensorError(int desiredSensor, int actualSensor);
+    void readCalibratedSensors();
+    int getIntensity(int ballAngle);
+
+
+
+
+private:
+    static const int sensorReadCount = 25;
+    RunningMedian s1Distance, s2Distance, s3Distance, s4Distance, s5Distance, s6Distance, s7Distance, s8Distance, s9Distance, s10Distance, s11Distance, s12Distance;
+    int sensorValues[12];
+};
+
+#endif
